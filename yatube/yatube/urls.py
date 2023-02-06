665dda686f458f4994b55dc28b909df2e6618b51
Airat <airatshaiahmetov@yandex.ru>
2023-02-06 22:49:25 +0300

    12

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -17,5 +17,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('', include('posts.urls')),
+    path('group/<slug:slug>/', include('posts.urls')),
     path('admin/', admin.site.urls),
 ]
