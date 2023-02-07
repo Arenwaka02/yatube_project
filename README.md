a00bbf12623b801ec2ce5c7b8e9daa84b3c37832
Airat <airatshaiahmetov@yandex.ru>
2023-02-07 13:19:36 +0300

    First

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # yatube_project
 ## Описание 
 Социальная сеть блогеров
-### Технологии
-Python 3.9
-Django
-### Запуск проекта в dev-режиме
+## Технологии
+- Python 3.9
+- Django
+
+## Запуск проекта в dev-режиме 
 - Установите и активируйте виртуальное окружение
 - Установите зависимости из файла requirements.txt
 ```
@@ -15,4 +16,4 @@ pip install -r requirements.txt
 python3 manage.py runserver
 ```
 ### Авторы
-S.A. Airat
+*S.A. Airat*
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*6#xpf_4mfyw$c@c%%i!*ln9-j#6vr#rs)^z4rfa3+d!r3vpw7'
+SECRET_KEY = 'm$49^w^r3$-i$)h!gbu&f&%a36ns(#5%cawd778137+impdaui'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'posts.apps.PostsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
