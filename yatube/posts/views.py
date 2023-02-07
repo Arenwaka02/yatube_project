e87723621106261fad11536c34713c642112bfa5
Airat <airatshaiahmetov@yandex.ru>
2023-02-07 14:33:35 +0300

    122

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse('Главная страница')
+
+
+def group_posts(requests, slug):
+    return HttpResponse(f'Посты {slug}')
+
+
+def group_posts_list(requests):
+    return HttpResponse('Посты')
